c8c274c779ad2162523a8adeb697eb09ef91b6f8
MarshTheBacca <marshallhunt@live.com>
2024-07-29 21:28:22 +0100

    struct definition of settings data class, used to store the settings of the game

---
+++ include/settings_data.h
@@ -0,0 +1,19 @@
+struct SettingsData {
+  int length = 5;
+  int width = 5;
+  int numDots = 3;
+  int numInitialPowerups = 5;
+  int powerupPlacementFrequency = 3;
+  int numInitialCrumblies = 3;
+  int barrierDensity = 4;
+  int numDeletes = 3;
+  int numCreates = 3;
+
+  /**
+   * @brief Construct a new Settings Data object
+   */
+  SettingsData() = default;
+
+  void edit();
+  void print() const;
+};
