e335728cde4baca51c69f3eca485adb1c69c7a9e
MarshTheBacca <marshallhunt@live.com>
2024-07-29 21:27:53 +0100

    header file for validation tools to declare functions

---
+++ include/validation_tools.h
@@ -0,0 +1,13 @@
+#include <optional>
+#include <string>
+
+int getValidInt(const std::string &prompt, const int lower, const int upper);
+bool confirm(const std::string &prompt);
+std::optional<std::string>
+getValidString(const std::string &prompt, const int lower, const int upper,
+               const std::string &cancelString,
+               const std::optional<std::string> &accepted = std::nullopt,
+               const std::optional<std::string> &forbidden = std::nullopt);
+std::optional<std::pair<int, int>>
+getValidCoord(const std::string &prompt, const int length, const int width,
+              const std::string &cancelString);
