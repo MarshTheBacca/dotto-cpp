c5bef358d4b0720b9dc52e7b7da6323e8342501b
MarshTheBacca <marshallhunt@live.com>
2024-07-30 02:55:04 +0100

    new 'random' class to generate random numbers using a singleton instance to avoid repeated code

---
+++ include/random.h
@@ -0,0 +1,32 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <random>
+
+/**
+ * @brief A class to generate random numbers using the Mersenne Twister algorithm
+ * and avoids repeated code for random number generation
+ */
+class Random {
+   public:
+    Random();
+
+    int getInt(int min, int max) const;
+    float getFloat(float min, float max) const;
+
+    // Public method to get singleton instance
+    // static so it is only defined ONCE in the program
+    static Random& getInstance();
+
+   private:
+    std::mt19937 generator;  // Mersenne Twister random number generator
+
+    // Delete copy constructor and assignment operator to prevent copying
+    Random(const Random&) = delete;
+    Random& operator=(const Random&) = delete;
+
+    // define static member variable for the singleton instance
+    static Random instance;
+};
+
+#endif  // RANDOM_H
---
+++ src/random.cpp
@@ -0,0 +1,38 @@
+#include "random.h"
+
+/**
+ * @brief Construct a new Random::Random object
+ * Initialize the random number generator with a seed
+ * The seed is generated using std::random_device
+ */
+Random::Random() : generator(std::random_device{}()) {}
+
+/**
+ * @brief Get the singleton instance
+ * @return The singleton instance
+ */
+Random& Random::getInstance() {
+    return instance;
+}
+
+/**
+ * @brief Get a random integer
+ * @param min The minimum value
+ * @param max The maximum value
+ * @return A random integer
+ */
+int Random::getInt(int min, int max) const {
+    std::uniform_int_distribution dist(min, max);
+    return dist(generator);
+}
+
+/**
+ * @brief Get a random float
+ * @param min The minimum value
+ * @param max The maximum value
+ * @return A random float
+ */
+float Random::getFloat(float min, float max) const {
+    std::uniform_real_distribution dist(min, max);
+    return dist(generator);
+}
