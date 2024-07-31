84db4dcfb8e491c0f53395d9bff2d75999637399
MarshTheBacca <marshallhunt@live.com>
2024-07-31 14:29:57 +0100

    changes several types from strings to chars, vectors to sets etc.

--- include/validation_tools.h
+++ include/validation_tools.h
@@ -2,17 +2,16 @@
 #define VALIDATION_TOOLS_H
 
 #include <optional>
+#include <set>
 #include <string>
 
 int getValidInt(const std::string &prompt, const int lower, const int upper);
 bool confirm(const std::string &prompt);
-std::optional<std::string>
-getValidString(const std::string &prompt, const int lower, const int upper,
-               const std::string &cancelString,
-               const std::optional<std::string> &accepted = std::nullopt,
-               const std::optional<std::string> &forbidden = std::nullopt);
+std::optional<std::string> getValidString(const std::string &prompt, const int lower, const int upper,
+                                          const std::optional<std::string_view> &cancelString = std::nullopt,
+                                          const std::optional<std::set<char>> &accepted = std::nullopt,
+                                          const std::optional<std::set<char>> &forbidden = std::nullopt);
 std::optional<std::pair<int, int>>
-getValidCoord(const std::string &prompt, const int length, const int width,
-              const std::string &cancelString);
+getValidCoord(const std::string &prompt, const int length, const int width);
 
 #endif  // VALIDATION_TOOLS_H
