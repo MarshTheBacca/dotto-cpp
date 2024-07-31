21aab05fe5373dcc5dc0fe22fda213e0f563e084
MarshTheBacca <marshallhunt@live.com>
2024-07-31 14:27:09 +0100

    added operators == and < for comparison so Portals may be stored in sets.

--- include/portal.h
+++ include/portal.h
@@ -7,8 +7,8 @@
  * @brief A struct to represent a portal pair
  */
 struct Portal {
-    std::pair<int, int> coord_1;
-    std::pair<int, int> coord_2;
+    std::pair<int, int> coord_1;  // first portal coordinate
+    std::pair<int, int> coord_2;  // second portal coordinate
 
     /**
      * @brief Construct a new Portal object
@@ -36,6 +36,24 @@ struct Portal {
     inline std::pair<int, int> getOpposite(std::pair<int, int> coord) const {
         return coord == coord_2 ? coord_1 : coord_2;
     }
+
+    /**
+     * @brief operator== overload to compare two portals
+     * @param other The other portal to compare to
+     * @return True if the portals are equal, false otherwise
+     */
+    // we can use the default implementation of operator== because we only have
+    // two member variables
+    bool operator==(const Portal &other) const = default;
+
+    /**
+     * @brief operator< overload to compare two portals
+     * @param other The other portal to compare to
+     * @return True if this portal is less than the other portal, false otherwise
+     */
+    bool operator<(const Portal &other) const {
+        return coord_1 < other.coord_1 || (coord_1 == other.coord_1 && coord_2 < other.coord_2);
+    };
 };
 
 #endif  // PORTAL_H
