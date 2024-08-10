493850b78b373ed2be968c1e284fa8b062e7958a
MarshTheBacca <marshallhunt@live.com>
2024-08-10 19:39:56 +0100

    moved definitions of constant cells to cell.h. added and edited several function definitions to match source file

--- include/board.h
+++ include/board.h
@@ -9,30 +9,19 @@
 #include "portal.h"
 #include "settings_data.h"
 
-const Cell BLANK_CELL = Cell(' ', RESET);
-const Cell REGULAR_CELL = Cell('/', RESET);
-const Cell PLAYER_1_CELL = Cell('O', BLUE);
-const Cell PLAYER_2_CELL = Cell('O', RED);
-const Cell POWERUP_CELL = Cell('?', PINK);
-const Cell BARRIER_CELL = Cell('#', RESET);
-const Cell CRUMBLY_CELL = Cell('~', RESET);
-const Cell PORTAL_CELL = Cell('@', PURPLE);
-
 struct Board {
     std::vector<std::vector<Cell>> field;
     const int length;
     const int width;
-    std::set<std::pair<int, int>> crumbliesCoords;
-    std::set<std::pair<int, int>> powerupCoords;
-    std::set<std::pair<int, int>> barrierCoords;
-    std::set<Portal> portals;
-    std::map<int, std::set<std::pair<int, int>>> dotCoords;
 
     explicit Board(SettingsData const &settingsData);
 
-    std::set<std::pair<int, int>> scanCellCoords(const Cell &targetCell) const;
+    std::vector<std::vector<Cell>> generateRandomMap(const SettingsData &settingsData) const;
+    std::set<std::pair<int, int>> scanCells(const Cell &targetCell) const;
+    std::set<std::pair<int, int>> scanCells(const std::set<Cell> &targetCells) const;
+
     void placePowerup();
-    void replaceCell(std::pair<int, int> coord, const Cell &newCell);
+    void replaceCell(const std::pair<int, int> &coord, const Cell &newCell);
     bool isWithinBounds(const std::pair<int, int> &coord) const;
     void show() const;
     Cell getCell(const std::pair<int, int> &coord) const;
