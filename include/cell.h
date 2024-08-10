71476eb6cbef16df67378d134fe886c6362bfa96
MarshTheBacca <marshallhunt@live.com>
2024-08-10 19:40:28 +0100

    added static qualifier to colours, and added static constant cell definitions

--- include/cell.h
+++ include/cell.h
@@ -4,12 +4,12 @@
 #include <string>
 
 // define colours
-const std::string RED = "\033[31m";
-const std::string BLUE = "\033[34m";
-const std::string GREEN = "\033[32m";
-const std::string PINK = "\033[95m";
-const std::string PURPLE = "\033[35m";
-const std::string RESET = "\033[0m";  // reset character to disable colour
+static const std::string RED = "\033[31m";
+static const std::string BLUE = "\033[34m";
+static const std::string GREEN = "\033[32m";
+static const std::string PINK = "\033[95m";
+static const std::string PURPLE = "\033[35m";
+static const std::string RESET = "\033[0m";  // reset character to disable colour
 
 struct Cell {
     char character;
@@ -32,5 +32,22 @@ struct Cell {
         return colour < other.colour;
     }
 };
+
+Cell charToCell(const char &character);
+
+static const Cell BLANK_CELL = Cell(' ', RESET);
+static const Cell REGULAR_CELL = Cell('/', RESET);
+static const Cell PLAYER_1_CELL = Cell('O', BLUE);
+static const Cell PLAYER_2_CELL = Cell('O', RED);
+static const Cell POWERUP_SOURCE_CELL = Cell('S', PINK);
+static const Cell BARRIER_CELL = Cell('#', RESET);
+static const Cell CRUMBLY_CELL = Cell('~', RESET);
+static const Cell PORTAL_CELL = Cell('@', PURPLE);
+static const Cell HOP_CELL = Cell('H', PINK);
+static const Cell PORTAL_POWER_CELL = Cell('P', PINK);
+static const Cell DESTROYER_CELL = Cell('D', PINK);
+static const Cell BISHOP_POWER_CELL = Cell('B', PINK);
+static const Cell BISHOP_1_CELL = Cell('!', BLUE);
+static const Cell BISHOP_2_CELL = Cell('!', RED);
 
 #endif  // CELL_H
