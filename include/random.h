0e65bfe86e4caf507a2344e41e2db836c7110fb0
MarshTheBacca <marshallhunt@live.com>
2024-08-10 19:49:30 +0100

    added shuffleVector and getRandomElement for both sets and vectors

--- include/random.h
+++ include/random.h
@@ -1,7 +1,11 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+#include <algorithm>  // std::shuffle
 #include <random>
+#include <ranges>
+#include <set>
+#include <vector>
 
 /**
  * @brief A class to generate random numbers using the Mersenne Twister algorithm
@@ -13,6 +17,40 @@ class Random {
 
     int getInt(int min, int max);
     float getFloat(float min, float max);
+
+    /**
+     * @brief Shuffle a vector
+     * @tparam T The type of the elements
+     */
+    template <typename T>
+    void shuffleVector(std::vector<T>& vector) {
+        std::ranges::shuffle(vector, generator);
+    }
+
+    // define template functions inside header file
+    /**
+     * @brief Get a random element from a vector
+     * @tparam T The type of the elements
+     * @param elements The vector of elements
+     * @return A random element
+     */
+    template <typename T>
+    T getRandomElement(const std::vector<T>& elements) {
+        return elements[getInt(0, elements.size() - 1)];
+    }
+
+    /**
+     * @brief Get a random element from a set
+     * @tparam T The type of the elements
+     * @param elements The set of elements
+     * @return A random element
+     */
+    template <typename T>
+    T getRandomElement(const std::set<T>& elements) {
+        // Convert to vector and call the other function
+        std::vector<T> vec(elements.begin(), elements.end());
+        return getRandomElement(vec);
+    }
 
     // Public method to get singleton instance
     // static so it is only defined ONCE in the program
